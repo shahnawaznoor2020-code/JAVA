a93da10b395ca906df96a93cc47da82af7ff0552
Shahnawaz Noor <shahnawaznoor2020@gmail.com>
2026-07-02 20:58:57 +0530

    Task Done

---
+++ Pattern_Printing/AlphabetSquare3.java
@@ -0,0 +1,24 @@
+package Pattern_Printing;
+
+import java.util.Scanner;
+
+public class AlphabetSquare3
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n=sc.nextInt();
+        for (int i=1;i<=n;i++)
+        {
+            for (int j=1;j<=n;j++)
+            {
+                if (i%2!=0)
+                    System.out.print((char)(i+96)+" ");
+                else
+                    System.out.print((char)(i+64)+" ");
+            }
+            System.out.println();
+        }
+    }
+}
