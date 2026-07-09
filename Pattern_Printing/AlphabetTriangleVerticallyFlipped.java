7ede20d096cbaba7a225f39cfc7e6169e4ec7251
Shahnawaz Noor <shahnawaznoor2020@gmail.com>
2026-07-09 17:19:33 +0530

    Task Done

---
+++ Pattern_Printing/AlphabetTriangleVerticallyFlipped.java
@@ -0,0 +1,25 @@
+package Pattern_Printing;
+
+import java.util.Scanner;
+
+public class AlphabetTriangleVerticallyFlipped
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n=sc.nextInt();
+        for (int i=1;i<=n;i++)
+        {
+            for (int j=1;j<=(n-i);j++)
+            {
+                System.out.print("  ");
+            }
+            for (int j=1;j<=i;j++)
+            {
+                System.out.print((char)(i+64)+" ");
+            }
+            System.out.println();
+        }
+    }
+}
